a6f4c6c41cb1a45d22ae47e5ceee3da570f0838b
Deadpoolshyam <90575203+Deadpoolshyam@users.noreply.github.com>
2022-12-24 15:32:52 +0530

    Add files via upload

---
+++ DB.js
@@ -0,0 +1,107 @@
+let musicDB = [
+    {
+      name: "Pilla Padesaave", 
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2001%20-%20Pilla%20Padesaave.mp3"
+    },
+    {
+      name: "Pranam Pothunna",
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2002%20-%20Pranam%20Pothunna.mp3"
+    },
+    {
+      name: "Bangu Aaku Thechi",
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2003%20-%20Bangu%20Aaku%20Thechi.mp3"
+    },
+    {
+      name: "Paathashalaloo",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2001%20-%20Paathashalaloo.mp3"
+    },
+    {
+      name: "Avunanavaa",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2002%20-%20Avunanavaa.mp3"
+    },
+    {
+      name: "Gundellonaa",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2003%20-%20Gundellonaa.mp3"
+    },
+    {
+      name: "Marachipolene",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2004%20-%20Marachipolene.mp3"
+    },
+    {
+      name: "Monnalaage",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2005%20-%20Ninna%20Monnalaage.mp3"
+    },
+    {
+      name: "Yendhi Ra Life Idhi",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2006%20-%20Yendhi%20Ra%20Life%20Idhi.mp3"
+    },
+    {
+      name: "Ola Olaala Ala",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2001%20-%20Ola%20Olaala%20Ala.mp3"
+    },
+    {
+      name: "Chilipiga",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2002%20-%20Chilipiga.mp3`"
+    },
+    {
+      name: "Nenu Nuvvantu",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2003%20-%20Nenu%20Nuvvantu.mp3"
+    },
+    {
+      name: "Hello Rammante",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2004%20-%20Hello%20Rammante.mp3"
+    },
+    {
+      name: "Orange",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2005%20-%20Orange.mp3"
+    },
+    {
+      name: "Rooba Rooba",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2006%20-%20Rooba%20Rooba.mp3"
+    },
+  ];
---
+++ script.js
@@ -0,0 +1,513 @@
+
+
+let musicDB = [
+    {
+      name: "Pilla Padesaave", 
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2001%20-%20Pilla%20Padesaave.mp3"
+    },
+    {
+      name: "Pranam Pothunna",
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2002%20-%20Pranam%20Pothunna.mp3"
+    },
+    {
+      name: "Bangu Aaku Thechi",
+      artist: "Yuvan Shankar Raja",
+      movie: "Love Today",
+      img: "https://naasongs.co/wp-content/uploads/2022/11/Love-Today-Telugu-2022-250x250.jpg",
+      song: "https://s093.naasongs.download/Love%20Today%20Telugu%20-%20(2022)/[iSongs.info]%2003%20-%20Bangu%20Aaku%20Thechi.mp3"
+    },
+    {
+      name: "Paathashalaloo",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2001%20-%20Paathashalaloo.mp3"
+    },
+    {
+      name: "Avunanavaa",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2002%20-%20Avunanavaa.mp3"
+    },
+    {
+      name: "Gundellonaa",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2003%20-%20Gundellonaa.mp3"
+    },
+    {
+      name: "Marachipolene",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2004%20-%20Marachipolene.mp3"
+    },
+    {
+      name: "Monnalaage",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2005%20-%20Ninna%20Monnalaage.mp3"
+    },
+    {
+      name: "Yendhi Ra Life Idhi",
+      movie: "Ori Devuda",
+      artist: "Vishwak Sen, Mithila Palkar, Venkatesh Kakumanu",
+      img: "https://naasongs.co/wp-content/uploads/2022/10/Ori-Devuda-2022-250x250.jpg",
+      song: "https://s091.naasongs.download/Ori%20Devuda%20-%20(2022)/[iSongs.info]%2006%20-%20Yendhi%20Ra%20Life%20Idhi.mp3"
+    },
+    {
+      name: "Ola Olaala Ala",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2001%20-%20Ola%20Olaala%20Ala.mp3"
+    },
+    {
+      name: "Chilipiga",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2002%20-%20Chilipiga.mp3"
+    },
+    {
+      name: "Nenu Nuvvantu",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2003%20-%20Nenu%20Nuvvantu.mp3"
+    },
+    {
+      name: "Hello Rammante",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2004%20-%20Hello%20Rammante.mp3"
+    },
+    {
+      name: "Orange",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2005%20-%20Orange.mp3"
+    },
+    {
+      name: "Rooba Rooba",
+      movie: "Orange",
+      artist: "Ram Charan Tej, Genelia D’Souza",
+      img: "https://ww1.naasongs.app/wp-content/uploads/2014/03/Orange-2010-250x250.jpg",
+      song: "https://s011.naasongs.download/Orange%20-%20%282010%29/%5BiSongs.info%5D%2006%20-%20Rooba%20Rooba.mp3"
+    },
+  ];
+
+let hindimusicList = [
+	{
+    name: "Raatan Lambiyan", 
+    artist: "Jubin Nautiyal, Asees Kaur",
+    movie: "Shershaah",
+    img: "https://c.saavncdn.com/238/Shershaah-Original-Motion-Picture-Soundtrack--Hindi-2021-20210815181610-500x500.jpg",
+    song: "https://pagalworld.com.se/files/download/id/3198"
+  },
+{
+    name: "Agar Tum Saath", 
+    artist: "Arijit Singh, Alka Yagnik",
+    movie: "Tamasha",
+    img: "https://t2.genius.com/unsafe/213x213/https%3A%2F%2Fimages.genius.com%2F194c071eed311cf4bf6673cb502365ec.1000x1000x1.jpg",
+    song: "https://pagalworld.com.se/files/download/id/2811"
+  },
+{
+    name: "Bekhayali", 
+    artist: "Sachet Tandon",
+    movie: "Bekhayali",
+    img: "https://i.scdn.co/image/ab67616d00001e02c5545f737b16ad5ee767b62a",
+    song: "https://pagalworld.com.se/files/download/id/3681"
+  },
+{
+    name: "Sanam Re", 
+    artist: "Arijit Singh, Mithoon",
+    movie: "Sanam Re",
+    img: "https://c.saavncdn.com/829/Sanam-Re-Hindi-2015-500x500.jpg",
+    song: "https://pagalworld.com.se/files/download/id/2818"
+  },
+{
+    name: "Param Sundari", 
+    artist: "Shreya Ghoshal",
+    movie: "Param Sundari",
+    img: "https://c.saavncdn.com/875/Mimi-Hindi-2021-20210715171822-500x500.jpg",
+    song: "https://pagalworld.com.se/files/download/id/3020"
+  },
+];
+// let icons = document.querySelector('.icons');
+// let navigatiosideBar = document.querySelector('.navigatiosideBar');
+// let iconsbg = document.querySelector('.iconsbg');
+
+// icons.addEventListener('click', ()=> {
+//     navigatiosideBar.classList.toggle('closeopen');
+//     // iconsbg.style.left = '250px';
+// });
+
+// console.log((musicDB[1] - 1).name);
+
+let DOMClassList = (()=> {
+
+    let DOMclass = {
+        icons : '.icons',
+        navigatiosideBar : '.navigatiosideBar',
+        iconsbg : '.iconsbg',
+        newSongsList : '.newSongsList',
+        newnxt: '#newnxt',
+        newprev: '#newprev',
+        oldprev: '#oldprev',
+        oldnxt: '#oldnxt',
+        recentprev: '#recentprev',
+        recentnxt: '#recentnxt',
+        audio: '.audio',
+        songDataimg : '.songDataimg',
+        song_title : '.song_title',
+        song_subTitle : '.song_subTitle',
+        playsongicon : '.playsongicon',
+        processBarWid : '.processBar',
+        startTime : '.startTime',
+        endTime : '.endTime',
+        process : '.process',
+        prevSong : '.prevSong',
+        nextSong : '.nextSong',
+        repeaticons : '.repeaticons',
+        repeatIcon : '.repeatIcon',
+        oldSongsList : '.oldSongsList'
+    }
+
+    return {
+        items : {
+            icons : document.querySelector(DOMclass.icons),
+            navigatiosideBar : document.querySelector(DOMclass.navigatiosideBar),
+            iconsbg : document.querySelector(DOMclass.iconsbg),
+            newlist : document.querySelector(DOMclass.newSongsList),
+            newnxt : document.getElementById(DOMclass.newnxt),
+            newprev : document.getElementById(DOMclass.newprev),
+            oldnxt : document.getElementById(DOMclass.oldnxt),
+            oldprev : document.getElementById(DOMclass.oldprev),
+            recentnxt : document.getElementById(DOMclass.recentnxt),
+            recentprev : document.getElementById(DOMclass.recentprev),
+            audio : document.querySelector(DOMclass.audio),
+            songDataimg : document.querySelector(DOMclass.songDataimg),
+            song_title : document.querySelector(DOMclass.song_title),
+            song_subTitle : document.querySelector(DOMclass.song_subTitle),
+            playsongicon : document.querySelector(DOMclass.playsongicon),
+            startTime : document.querySelector(DOMclass.startTime),
+            endTime : document.querySelector(DOMclass.endTime),
+            prevSong : document.querySelector(DOMclass.prevSong),
+            nextSong : document.querySelector(DOMclass.nextSong),
+            repeaticons : document.querySelector(DOMclass.repeaticons),
+            repeatIcon : document.querySelector(DOMclass.repeatIcon),
+            bollylist : document.querySelector(DOMclass.oldSongsList),
+        },
+
+        newSongs : (musicDB) => {
+            let HTML;
+
+            musicDB.forEach((data, index) => {
+
+                HTML = `
+                        <div class="smallsongcard newSongdatacards" id="${index}">
+                            <img class="smallimg" src="${data.img}" />
+                            <div class="titlesubtitle">
+                                <div class="songName">${data.name}</div>
+                                <div class="movieName">${data.movie}</div>
+                            </div>
+                        </div>
+                    `;
+                document.querySelector(DOMclass.newSongsList).innerHTML += HTML;
+            });
+
+        },
+
+        bollySongs : (hindimusicList) => {
+          let bollyHTML;
+
+          hindimusicList.forEach((data, index) => {
+
+            bollyHTML = `
+                        <div class="smallsongcard" id="${index}">
+                            <img class="smallimg" src="${data.img}" />
+                            <div class="titlesubtitle">
+                                <div class="songName">${data.name}</div>
+                                <div class="movieName">${data.movie}</div>
+                            </div>
+                        </div>
+                  `;
+              document.querySelector(DOMclass.oldSongsList).innerHTML += bollyHTML;
+          });
+
+      },
+
+        nextPrev : () => {
+            let newSongsList = document.getElementsByClassName('newSongsList')[0];
+            let oldSongsList = document.getElementsByClassName('oldSongsList')[0];
+            let recentSongsList = document.getElementsByClassName('recentSongsList')[0];
+            newnxt.addEventListener('click', () => {
+                newSongsList.scrollLeft += 330;
+            });
+
+            newprev.addEventListener('click', () => {
+                newSongsList.scrollLeft -= 330;
+            });
+
+            oldnxt.addEventListener('click', () => {
+                oldSongsList.scrollLeft += 330;
+            });
+
+            oldprev.addEventListener('click', () => {
+                oldSongsList.scrollLeft -= 330;
+            });
+
+            recentnxt.addEventListener('click', () => {
+                recentSongsList.scrollLeft += 330;
+            });
+
+            recentprev.addEventListener('click', () => {
+                recentSongsList.scrollLeft -= 330;
+            });
+        },
+
+        processBar : (currentTime, duration) => {
+
+          let processBarWid, audioPlayer, startTime, endTime, process;
+          processBarWid = document.querySelector(DOMclass.processBarWid);
+          audioPlayer = document.querySelector(DOMclass.audio);
+          startTime = document.querySelector(DOMclass.startTime);
+          endTime = document.querySelector(DOMclass.endTime);
+          process = document.querySelector(DOMclass.process);
+          let processBar = (currentTime/duration) * 100;
+          processBarWid.style.width = `${processBar}%`;
+
+          audioPlayer.addEventListener('loadeddata', ()=> {
+            let mainAudioDuration = audioPlayer.duration;
+            let totalMin = Math.floor(mainAudioDuration / 60);
+            let totalSec = Math.floor(mainAudioDuration % 60);
+            if(totalSec < 10) {
+              totalSec = `0${totalSec}`;
+            }
+            endTime.textContent = `${totalMin}:${totalSec}`;
+          });
+
+          let currentMin = Math.floor(currentTime / 60);
+          let currentSec = Math.floor(currentTime % 60);
+          if(currentSec < 10) {
+            currentSec = `0${currentSec}`;
+          }
+          startTime.textContent = `${currentMin}:${currentSec}`;
+
+          process.addEventListener('click', (e)=> {
+            let progressWidthval = process.clientWidth;
+            // console.log(progressWidthval);
+            let clickoffsetX = e.offsetX;
+            // console.log(clickoffsetX);
+            let songDuration = audioPlayer.duration;
+            audioPlayer.currentTime = (clickoffsetX / progressWidthval) * songDuration;
+          });
+
+        }
+    }
+
+})();
+
+
+let basicOperations = ((DOMClassList, musicDB, hindimusicList )=> {
+    
+    let playing, classes, newSongList, nextPrevBtn, ids;
+    playing = false;
+    classes = DOMClassList.items;
+    newSongList = DOMClassList.newSongs;
+    bollywoodSongList = DOMClassList.bollySongs;
+    nextPrevBtn = DOMClassList.nextPrev;
+    
+    ids = 0;
+    
+    let firstPlay = (ids)=> {
+        classes.audio.src = `${musicDB[ids].song}`;
+        classes.songDataimg.src = `${musicDB[ids].img}`;
+        classes.song_title.textContent = `${musicDB[ids].name}`;
+        classes.song_subTitle.textContent = `${musicDB[ids].artist}`;
+    };
+
+    let mainRun = () => {
+        classes.icons.addEventListener('click', navibarAction);
+        // console.log(musicDB[0]);
+        newSongList(musicDB);
+        bollywoodSongList(hindimusicList);
+        classes.newlist.addEventListener('click', playSong);
+        classes.bollylist.addEventListener('click', playbollySong);
+        classes.playsongicon.addEventListener('click', ()=> (playing ? pauseCurrentSong() : playCurrentSong()));
+        classes.audio.addEventListener('timeupdate', getstartendTimes);
+        classes.nextSong.addEventListener('click', nextSong);
+        classes.prevSong.addEventListener('click', prevSong);
+
+        classes.repeatIcon.addEventListener('click', switchIcons);
+
+        classes.audio.addEventListener("ended", ()=> {
+          
+          const repeatBtn = classes.repeatIcon;
+          let getText = repeatBtn.textContent;
+
+          switch(getText){
+            case "repeat":
+              nextSong();
+              break;
+            case "repeat_one":
+              classes.audio.currentTime = 0;
+              firstPlay(ids);
+              playCurrentSong();
+              break;
+            case "shuffle":
+              let randomIndex = Math.floor((Math.random() * musicDB.length) + 1);
+              do{
+                randomIndex = Math.floor((Math.random() * musicDB.length) + 1);
+              }while(ids == randomIndex);
+              ids = randomIndex;
+              firstPlay(ids);
+              nextSong();
+              break;
+          }
+
+        });
+
+    };
+    
+    let getstartendTimes = (e) => {
+      // console.log(e.target.currentTime);
+      const currentTime = e.target.currentTime;
+      const duration = e.target.duration;
+      DOMClassList.processBar(currentTime, duration);
+      // if(currentTime === duration){
+      //     nextSong();
+      // }
+    };
+
+    let pauseCurrentSong = () => {
+        classes.audio.pause();
+        playing = false;
+        classes.playsongicon.innerHTML = `<ion-icon name="play-circle-outline"></ion-icon>`;
+    };
+
+    let playCurrentSong = () => {
+        classes.audio.play();
+        playing = true;
+        classes.playsongicon.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+    };
+
+    let navibarAction = () => classes.navigatiosideBar.classList.toggle('closeopen');
+    // classes.icons.classList.toggle('closeopen');
+
+    let nextSong = (e) => {
+      ids++;
+      // console.log(ids);
+      ids > (musicDB.length - 1) ?  ids = 0 : ids = ids;
+        classes.audio.src = `${musicDB[ids].song}`;
+        classes.songDataimg.src = `${musicDB[ids].img}`;
+        classes.song_title.textContent = `${musicDB[ids].name}`;
+        classes.song_subTitle.textContent = `${musicDB[ids].artist}`;
+        classes.audio.play();
+        classes.playsongicon.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+    };
+
+    let prevSong = (e) => {
+      ids--;
+      // console.log(ids);
+      ids < 0 ? ids = (musicDB.length - 1) : ids = ids;
+        classes.audio.src = `${musicDB[ids].song}`;
+        classes.songDataimg.src = `${musicDB[ids].img}`;
+        classes.song_title.textContent = `${musicDB[ids].name}`;
+        classes.song_subTitle.textContent = `${musicDB[ids].artist}`;
+        classes.audio.play();
+        classes.playsongicon.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+    };
+
+    let playSong = (e) =>{
+        // console.log(e.target.parentNode.id);
+        ids = e.target.parentNode.id;
+        // console.log(`${musicDB[ids].song}`);
+        classes.audio.src = `${musicDB[ids].song}`;
+        classes.songDataimg.src = `${musicDB[ids].img}`;
+        classes.song_title.textContent = `${musicDB[ids].name}`;
+        classes.song_subTitle.textContent = `${musicDB[ids].artist}`;
+        classes.audio.play();
+        classes.playsongicon.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+        playing = true;
+        
+        
+    }; 
+
+
+    let playbollySong = (e) =>{
+        // console.log(e.target.parentNode.id);
+        ids = e.target.parentNode.id;
+        // console.log(`${musicDB[ids].song}`);
+        classes.audio.src = `${hindimusicList[ids].song}`;
+        classes.songDataimg.src = `${hindimusicList[ids].img}`;
+        classes.song_title.textContent = `${hindimusicList[ids].name}`;
+        classes.song_subTitle.textContent = `${hindimusicList[ids].artist}`;
+        classes.audio.play();
+        classes.playsongicon.innerHTML = `<ion-icon name="pause-outline"></ion-icon>`;
+        playing = true;
+    }; 
+
+
+    let switchIcons = () => {
+      const repeatBtn = classes.repeatIcon;
+      let getText = repeatBtn.textContent;
+
+      switch(getText){
+        case "repeat":
+          repeatBtn.innerHTML = `<ion-icon name="reload-outline">repeat_one</ion-icon>`;
+          break;
+        case "repeat_one":
+          repeatBtn.innerHTML = `<ion-icon name="shuffle-outline">shuffle</ion-icon>`;
+          break;
+        case "shuffle":
+          repeatBtn.innerHTML = `<ion-icon name="repeat-outline">repeat</ion-icon>`;
+          break;
+      }
+    };
+
+
+    
+
+    return {
+        init : ()=> {
+            firstPlay(ids);
+            mainRun();
+            nextPrevBtn();
+        }
+    }
+
+})(DOMClassList, musicDB, hindimusicList );
+
+basicOperations.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
